4ce5feb823e98b929a87f0bc1453d31ddbfaeb88
keith <keith@218.237.249.172>
2014-04-21 22:45:43 +0900

    a

--- ONewWall/app.js
+++ ONewWall/app.js
@@ -11,11 +11,12 @@ var express = require('express'),
 	app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
+app.set('port', 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.logger('dev'));
+//app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
